54470511d943cc9982b888689a332bf10ea133e3
Prochy20 <prochy.20.m@seznam.cz>
2022-06-02 16:02:01 +0200

    initial commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
+<template>
+  <HeroBanner />
+  <div class="container mx-auto">
+    <Support />
+    <Blog />
+  </div>
+  <Contact />
+</template>
